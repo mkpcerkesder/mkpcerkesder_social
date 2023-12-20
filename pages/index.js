42011608e6314ec64f40968b6870b73867e99c3e
mkpcerkesder <154452126+mkpcerkesder@users.noreply.github.com>
2023-12-21 01:08:43 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,19 @@ export default function Home() {
               Dilek ve Öneri Formu
           </a>
         </div>
+        <div className="social-media-map">
+          <iframe 
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d190.92544470127564!2d28.38581829797479!3d40.03508571469377!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14b60d599cbf71e5%3A0xc7d1ba1bc36054db!2zTXVzdGFmYWtlbWFscGHFn2Egw4dlcmtlcyBLw7xsdMO8ciBEZXJuZcSfaQ!5e0!3m2!1sen!2str!4v1703109430837!5m2!1sen!2str" 
+            height="450" 
+            style={{ border: 0 }} 
+            allowfullscreen="" 
+            loading="lazy" 
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
       </main>
+      <footer>
+        <span>Copyright © 2023</span> <span>Mustafakemalpaşa Çerkes Kültür Derneği</span>
+      </footer>
     </>
   )
 }
